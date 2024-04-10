3585f81c0bf612ef363130ca4c4159515f79818e
chaitanya7517 <chaitanyanalawade84@gmail.com>
2024-04-10 18:34:52 +0530

    create sign in api route

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,19 +1,44 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
-export const signup = async (req,res,next) => {
-    const {username, email, password} = req.body;
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
 
-    const hashedPassword = await bcryptjs.hash(password,10);
-    
-    try{
-        const newUser = await User.create({username,email,password:hashedPassword});
-        res.status(201).json({
-                message: 'User Created Successfully',
-        })
-    }catch(err) {
-        next(err);
-    }
-    
-}
+  const hashedPassword = await bcryptjs.hash(password, 10);
+  //no need to check user is already present or not becoz in model we use unique key 
+  try {
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    res.status(201).json({
+      message: "User Created Successfully",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = await bcryptjs.compare(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest} = validUser._doc;
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
